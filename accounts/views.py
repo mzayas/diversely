b8fbaa1f914473d202e87f139f388eca9d8ae330
Matthew Zayas <matthew@diversely.nyc>
2018-09-20 02:27:32 -0400

    design work

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,22 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.http import HttpResponse
 
+from .models import User
+from .forms import RegisterForm
 # Create your views here.
+
+def RegisterView(request):
+    if request.method =='POST':
+        form = RegisterForm(request.POST)
+        if form.is_valid():
+            form.save()
+        return redirect('index')
+    else:
+        form = RegisterForm()
+
+        args = {'form': form}
+        return render(request, 'register.html', args)
+
+def login(request):
+    # return HttpResponse('home from Python!')
+    return render(request, 'login.html')    
--- diversely/urls.py
+++ diversely/urls.py
@@ -2,7 +2,8 @@ from django.urls import path
 from django.contrib import admin
 admin.autodiscover()
 
-import home.views, diagnostics.views, tools.views
+import home.views, diagnostics.views, tools.views, accounts.views
+
 
 # Examples:
 # url(r'^$', 'diversely.views.home', name='home'),
@@ -10,8 +11,9 @@ import home.views, diagnostics.views, tools.views
 
 urlpatterns = [
     path('', home.views.index, name='index'),
-    path('db', home.views.db, name='db'),
     path('tools', tools.views.tools, name='tools'),
     path('diagnostics', diagnostics.views.diagnostics, name='diagnostics'),
+    path('register', accounts.views.RegisterView, name='register'),
+    path('login', accounts.views.login, name='login'),
     path('admin/', admin.site.urls),
 ]
